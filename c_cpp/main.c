e6cc00706facdfb740f97dcb9d538ab2594041b9
shary <yylittlegirl@vip.qq.com>
2015-06-03 02:27:30 -0700

    all

---
+++ c_cpp/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "include/my_stack.h"
+
+int main(void) {
+        STACK s;
+        init(&s);
+             
+
+	int x;	
+
+        while ( ! stack_is_full() ) {
+		scanf("%d", &x);
+		stack_push(x);
+	}
+	
+
+	while ( !stack_is_empty() ) {
+		x = stack_pop();
+
+		printf("%d\t", x);
+	}
+	printf("\n");
+
+	return 0;
+}
