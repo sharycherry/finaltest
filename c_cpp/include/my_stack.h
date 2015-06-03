72ec91de8a9271b0b9ec472adac43e64cfd60511
shary <yylittlegirl@vip.qq.com>
2015-06-03 01:52:32 -0700

    all

---
+++ c_cpp/include/my_stack.h
@@ -0,0 +1,20 @@
+tack_push(int x);
+int stack_pop();
+
+// The capacity of the stack
+int stack_capacity();
+
+// Current available size of the stack
+int stack_size();
+
+
+/*
+ * return 0/1 to check if stack is empty or full
+ * 0 - No
+ * 1 - Yes
+ */
+int stack_is_empty();
+int stack_is_full();
+
+
+#endif //_MY_STACK_H
